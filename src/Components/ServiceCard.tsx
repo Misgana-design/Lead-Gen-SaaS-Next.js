ca00a8bb54cd4e76375ead24e008e1fbee45e44c
Misgana-design <misganawoldeselassie@gmail.com>
2026-01-22 22:04:26 +0300

    Reusable service card

--- src/Components/ServiceCard.tsx
+++ src/Components/ServiceCard.tsx
@@ -1,1 +1,18 @@
-export default function ServiceCard() {}
+export interface Service {
+  id?: number;
+  title: string;
+  description: string;
+  price: string;
+}
+
+export default function ServiceCard({ title, description, price }: Service) {
+  return (
+    <div className="w-90 bg-white rounded-2xl">
+      <div className="my-3 px-3 font-mono font-bold text-lg text-black">
+        {title}
+      </div>
+      <div className="my-3 px-3 font-mono text-black">{description}</div>
+      <div className="my-3 px-3 font-mono text-blue-500">{price}</div>
+    </div>
+  );
+}
