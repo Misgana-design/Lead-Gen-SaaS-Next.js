3a8e40feacb73aad16f253fb9b5b57ad5782ef53
Misgana-design <misganawoldeselassie@gmail.com>
2026-01-23 23:09:42 +0300

    Font service card

--- src/Components/ServiceCard.tsx
+++ src/Components/ServiceCard.tsx
@@ -8,9 +8,7 @@ export interface Service {
 export default function ServiceCard({ title, description, price }: Service) {
   return (
     <div className="w-90 bg-white rounded-2xl">
-      <div className="my-3 px-3 font-mono font-bold text-lg text-black">
-        {title}
-      </div>
+      <div className="my-3 px-3 font-bold text-lg text-black">{title}</div>
       <div className="my-3 px-3 font-mono text-black">{description}</div>
       <div className="my-3 px-3 font-mono text-blue-500">{price}</div>
     </div>
