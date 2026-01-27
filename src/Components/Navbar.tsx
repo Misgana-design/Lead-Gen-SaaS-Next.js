13e55bca92e9a92e19c85392ca5e33d6b885d284
Misgana-design <misganawoldeselassie@gmail.com>
2026-01-27 14:29:57 +0300

    The Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import BookingButton from "./BookingButton";
+import BookingButton from "@/Components/BookingButton";
 
 export default function Navbar() {
   return (
